91d79a89ab847f0c68420f695940dfbcdc7adc79
Natan Felles <natanfelles@gmail.com>
2017-08-01 20:26:51 -0300

    Update syntax and plugins calls

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//ini_set('display_errors', 0);
+ini_set('display_errors', 0);
 
 function adminer_object()
 {
@@ -13,29 +13,37 @@ function adminer_object()
 	}
 
 	// Specify enabled plugins here.
-	$plugins = array(
-		new AdminerLoginServers([
-			'localhost',
-			'192.168.2.2',
-			'192.168.2.3',
-		]),
-		new AdminerCollations([
-			'utf8_general_ci',
-			'latin1_swedish_ci',
-		]),
-		new AdminerDatabaseHide([
+	$plugins = [
+		/*new AdminerDatabaseHide([
 			'mysql',
 			'information_schema',
 			'performance_schema',
 			'sys',
-		]),
-		new AdminerSimpleMenu(),
+		]),*/
+
+		/*new AdminerLoginServersEnhanced([
+			new AdminerLoginServerEnhanced('127.0.0.1:3306', '127.0.0.1:3306 - MySQL', 'server'),
+			new AdminerLoginServerEnhanced('127.0.0.1:3307', '127.0.0.1:3307 - MariaDB', 'server'),
+			new AdminerLoginServerEnhanced('127.0.0.1:5432', '127.0.0.1:5432 - PostgreSQL', 'pgsql'),
+			new AdminerLoginServerEnhanced('127.0.0.1:5432', '127.0.0.1:27017 - MongoDB', 'mongo'),
+		]),*/
+
+		/*new AdminerCollations([
+			'utf8_general_ci',
+			'utf8mb4_general_ci',
+			'latin1_swedish_ci',
+		]),*/
+
 		new AdminerJsonPreview(),
-		//new AdminerISPConfig(),
+		new AdminerDumpJson(),
+
+		// new AdminerISPConfig(),
+
+		new AdminerSimpleMenu(),
 
 		// AdminerTheme has to be the last one.
 		new AdminerTheme('bootstrap-like'),
-	);
+	];
 
 	return new AdminerPlugin($plugins);
 }
