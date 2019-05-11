54681f8ac87519a631d7a654b0711be919099db1
Natan Felles <natanfelles@gmail.com>
2019-05-11 18:45:00 -0300

    Enable jush

--- compile.php
+++ compile.php
@@ -1,4 +1,6 @@
 <?php
+rmdir(__DIR__ . '/vendor/vrana/adminer/externals/jush');
+symlink(__DIR__ . '/vendor/vrana/jush', __DIR__ . '/vendor/vrana/adminer/externals/jush');
 require __DIR__ . '/vendor/vrana/adminer/compile.php';
 rename($filename, 'adminer.php');
 echo "{$filename} renamed to adminer.php.\n";
