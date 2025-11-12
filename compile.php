abd5e71e1a0c7c610895aa740d293f0b30fa20ea
Natan Felles <natanfelles@gmail.com>
2025-11-12 18:44:41 -0300

    Update comment

--- compile.php
+++ compile.php
@@ -2,7 +2,7 @@
 rmdir(__DIR__ . '/vendor/vrana/adminer/externals/jush');
 symlink(__DIR__ . '/vendor/vrana/jush', __DIR__ . '/vendor/vrana/adminer/externals/jush');
 
-// "vrana/jsshrink": "dev-main",
+//"vrana/jsshrink": "dev-main#4f8a69bd80bda24e8233bca428e5f56400e3cb1b",
 //rmdir(__DIR__ . '/vendor/vrana/adminer/externals/JsShrink');
 //symlink(__DIR__ . '/vendor/vrana/jsshrink', __DIR__ . '/vendor/vrana/adminer/externals/JsShrink');
 
